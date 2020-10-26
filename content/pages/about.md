93d9c7afb520f33cc60d18aa5fd4268999345f5b
Jim Anderson <jima.coding@gmail.com>
2020-10-25 18:51:14 -0600

    First source material for Pelican-based blog

---
+++ content/pages/about.md
@@ -0,0 +1,19 @@
+Title: About
+----------------------
+
+My name is Jim and I'm a well-seasoned (read: OLD) developer who has spent many years developing in C-like languages: C, C++, Java, Lua, Python, Perl, and a few others. Much of my experience is in embedded systems, most recently doing embedded Linux products.
+
+In my spare time I like to bike and snowboard, though rarely at the same time.
+
+I'm currently working on the image processing pipeline for video security cameras as my day job. I also am an editor and writer for [Real Python](https://realpython.com/team/janderson/) which lets me work on my writing and connects me with a bunch of fun people!
+
+## What's this blog about?
+
+In this blog, I plan on writing about Python, C/C++, debugging, office politics, and whatever is striking my fancy at the time. I have no planned schedule for posts, it's really whenever the mood strikes me. Really, this is just an outlet for some writing that doesn't fit the requirements for a Real Python article or is more about soft skills than technical ones.
+
+## What if I have a question or comment?
+
+Feel free to hit the contact links if you want to comment or discuss anything I write about here. I'll try to get back to you, provided you're not shouting.
+
+It'll be fun to see where this leads!
+
---
+++ pelicanconf.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Jim Anderson'
+SITENAME = 'Snowboarding Coder'
+SITESUBTITLE = 'Musing on writing code'
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'America/Denver'
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+    },
+    'output_format': 'html5',
+}
+
+
+PLUGINS = [
+            'minchin.pelican.plugins.nojekyll',
+          ]
+
+USE_FOLDER_AS_CATEGORY = False
+DEFAULT_CATEGORY = 'Posts'
+
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+
+# JHA TODO REmove this flag for Dev only
+# LOAD_CONTENT_CACHE = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = 'pelican-twitchy'
+PYGMENTS_STYLE = "xcode"
+BOOTSTRAP_THEME = 'darkly'
+EXPAND_LATEST_ON_INDEX = True
+SHARE = True
+SOCIAL = (('Twitter', 'https://twitter.com/jimande75053775'), ('GitHub', 'https://github.com/jima80525'))
+DISPLAY_CATEGORIES_ON_MENU = False
+CC_LICENSE = 'CC-BY-SA'
+
+# these were 2nd place themse for twitchy
+# BOOTSTRAP_THEME = 'cyborg'
+# BOOTSTRAP_THEME = 'litera'
+# BOOTSTRAP_THEME = 'slate'
